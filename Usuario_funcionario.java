ae870ffe606b8dde285d007d26ed1d56e5124968
lauccb <130602992+lauccb@users.noreply.github.com>
2023-05-02 20:05:13 -0300

    Add files via upload

---
+++ Usuario_funcionario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Primeiro_trab_prog;
+
+/**
+ *
+ * @author Laura Coelho
+ */
+public class Usuario_funcionario {
+    private String nome;
+    private String senha;
+    
+}
---
+++ Veiculo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Primeiro_trab_prog;
+
+/**
+ *
+ * @author Laura Coelho
+ */
+public class Veiculo {
+    
+}
